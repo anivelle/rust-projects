f5de9396953f4b824d9cc0b406ff08fa38acbee4
Alex Nivelle <anivelle@ucsc.edu>
2022-12-05 01:11:32 -0800

    Test project please ignore

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+mod custom_button;
+use custom_button::CustomButton;
+use gtk::prelude::*;
+use gtk::{self, Application, ApplicationWindow};
+
+
+const APP_ID: &str = "org.gtk_rs.HelloWorld1";
+fn main() {
+    let app = Application::builder().application_id(APP_ID).build();
+
+    app.connect_activate(build_ui);
+
+    app.run();
+}
+
+fn build_ui(app: &Application) {
+    let button = CustomButton::with_label("Press me!");
+    button.set_margin_top(12);
+    button.set_margin_bottom(12);
+    button.set_margin_start(12);
+    button.set_margin_end(12);
+
+    button.connect_clicked(move |button| {
+        button.set_label("Hello World!");
+    });
+
+
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("My GTK App")
+        .child(&button)
+        .build();
+
+    window.present();
+}
