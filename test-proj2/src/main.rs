14aa22fa9b5169ff64d95bef2eb344e30e9c1be1
Alex Nivelle <anivelle@ucsc.edu>
2022-12-06 00:54:39 -0800

    Forgot charger, no more Rust

--- test-proj2/src/main.rs
+++ test-proj2/src/main.rs
@@ -1,3 +1,18 @@
+use gtk::prelude::*;
+
+
 fn main() {
-    println!("Hello, world!");
+    let string_some_value = "Hello!".to_value();
+    let string_none_value = None::<String>.to_value();
+
+    let string_some = string_some_value
+        .get::<Option<String>>()
+        .expect("The value needs to be of type 'Option<String>'.");
+
+    let string_none = string_none_value
+        .get::<Option<String>>()
+        .expect("The value needs to be of type 'Option<String>'.");
+
+    assert_eq!(string_some, Some("Hello!".to_string()));
+    assert_eq!(string_none, None);
 }
