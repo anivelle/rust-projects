1cbd9225840ae3767649aaf418c72dcd78f23b02
Alex Nivelle <anivelle@ucsc.edu>
2023-01-03 01:44:01 -0800

    Need to figure out how to get keyboard events with the drawing area or something

--- cairo_practice/src/main.rs
+++ cairo_practice/src/main.rs
@@ -3,25 +3,49 @@ use gtk::gdk;
 use gtk::glib;
 use gtk::prelude::*;
 use gtk::{Application, ApplicationWindow, DrawingArea};
+use once_cell::sync::Lazy;
 
-const APP_ID: &str = "org.gtk_rs.HelloWorld2";
+const APP_ID: &str = "org.gtk_rs.cairo_practice";
+// Track window width and height (assumes window is never resized please never resize it outside of
+// these variables dear god)
+const WIDTH: i32 = 20 * 64;
+const HEIGHT: i32 = 20 * 32;
+// Static Cairo region to make XORing pixels for display a lot simpler
+static mut REGION: Lazy<cairo::Region> = Lazy::new(|| {
+    let region = cairo::Region::create_rectangle(&cairo::RectangleInt::new(0, 0, WIDTH, HEIGHT));
+    region
+});
+static mut X: i32 = 0;
+static mut Y: i32 = 0;
 
 fn draw_func(area: &DrawingArea, cr: &cairo::Context, width: i32, height: i32) {
-    println!("{} {}", width, height);
     let context = area.style_context();
-    let rect = cairo::Region::create_rectangle(&cairo::RectangleInt::new(20, 20, 100, 100));
+    let pixel_width = width / 64;
+    let pixel_height = height / 32;
+    let rect =
+        cairo::Region::create_rectangle(&cairo::RectangleInt::new(0, 0, pixel_width, pixel_height));
+    unsafe {
+        REGION.xor(&rect).unwrap();
+        if X == 64 {
+            X = 0;
+        } else if X == -1 {
+            X = 63;
+        }
+        if Y == 32 {
+            Y = 0;
+        } else if Y == -1 {
+            Y = 31;
+        }
+        rect.translate(
+            rect.rectangle(0).width() * X,
+            rect.rectangle(0).height() * Y,
+        );
+        REGION.xor(&rect).unwrap();
+    }
     let color = context.color();
-    cr.arc(
-        width as f64 / 2.0,
-        height as f64 / 2.0,
-        if width > height {
-            height as f64 / 2.0
-        } else {
-            width as f64 / 2.0
-        },
-        0.0,
-        2.0 * 3.1415926535,
-    );
+    unsafe {
+        cr.add_region(&REGION);
+    }
     cr.set_source_rgba(
         color.red().into(),
         color.green().into(),
@@ -44,18 +68,50 @@ fn main() {
 fn build_ui(app: &Application) {
     // Create a window and set the title
     let darea = DrawingArea::builder()
-        .content_width(400)
-        .content_height(400)
+        .width_request(WIDTH)
+        .height_request(HEIGHT)
         .build();
 
     darea.set_draw_func(draw_func);
+
+    // REPEATED EVENTS LETS GO
+    // gtk::glib::timeout_add(interval, func)
+    darea.queue_draw();
+    // Create the application window
     let window = ApplicationWindow::builder()
         .application(app)
         .title("My GTK App")
         .child(&darea)
         .build();
+    // Doesn't get events when the parent is the drawing area
+    let key = gtk::EventControllerKey::new();
+    // TODO: Figure out how to redraw the drawing area
+    key.connect_key_released(|key, keyval, _, _| {
+        println!("Key event captured");
+        // Move the little square around the screen hehe
+        match keyval {
+            gdk::Key::Left => unsafe {
+                X -= 1;
+            },
+            gdk::Key::Right => unsafe {
+                X += 1;
+            },
+            gdk::Key::Up => unsafe {
+                Y -= 1;
+            },
+            gdk::Key::Down => unsafe {
+                Y += 1;
+            },
+            _ => todo!(),
+        }
+        // Doesn't redraw drawing area, as you'd expect, but no parent exists
+        key.widget().queue_draw();
+    });
+    //darea.add_controller(&key);
+    window.add_controller(&key);
+
 
     // Present window
     window.show();
-    darea.queue_draw();
+    //darea.grab_focus();
 }
